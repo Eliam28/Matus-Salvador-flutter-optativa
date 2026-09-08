2a445391e610c7870ad305f8586cec9ac5f20324
Eliam28 <152578969+Eliam28@users.noreply.github.com>
2026-09-07 21:18:19 -0600

    Calculadora final
    - Widgets con estado
    - Logica acabada
    - Clases de props

---
+++ Parcial 1/tarea_2/lib/Props/NumInpProps.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Numinpprops {
+  final TextEditingController inputIzq;
+  final TextEditingController inputDer;
+  final FocusNode focusIzq;
+  final FocusNode focusDer;
+
+  const Numinpprops({required this.focusDer, required this.focusIzq, required this.inputDer, required this.inputIzq});
+
+}
